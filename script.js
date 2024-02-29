10ba58811429381e19950f3299ae157a70b3d09b
augustose <augustose@gmail.com>
2024-02-29 16:28:27 -0500

    indent work 01

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const menuIcon = document.querySelector('.menu-icon');
-const menuItems = document.querySelector('.menu-items');
-
-menuIcon.addEventListener('click', () => {
+const menuItems = document.querySelector('.menu-items');menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('active');
 });
